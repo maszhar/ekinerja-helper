58d87b6b51e2323ad372c038a0de8524ac1547c1
maszhar <fikrimustofa024@gmail.com>
2024-12-15 09:33:28 +0700

    feat: get token from ekinerja login web

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,27 @@ let mainWindow: BrowserWindow | null = null;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
-        width: 800,
+        width: 1024,
         height: 600,
     })
-
+    mainWindow.maximize()
     mainWindow.loadURL("https://kinerja.bkn.go.id/login")
+
+    mainWindow.webContents.once('did-finish-load', () => {
+        setInterval(() => {
+            mainWindow?.webContents.executeJavaScript('localStorage.getItem("token");')
+                .then((token) => {
+                    console.log("Token:", token)
+                })
+                .catch((e) => {
+                    console.error(e)
+                })
+        }, 3000)
+    })
 })
 
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin') {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 })
